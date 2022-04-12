289dcce97784308a77cc160497e0d51fc8a4d163
Aldrin Baroi <aldrin.baroi@gmail.com>
2022-04-12 05:58:29 +0000

    Updated to read AWS credentials from /.aws directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,9 @@ if [[ $CMD == "aws" ]]; then
 elif [[ $CMD == "console" && $ARG1 == "bash" ]]; then
 	bash
 else
-	AWS_ACCESS_KEY_ID=$(iniget /root/.aws/credentials $PROFILE aws_access_key_id)
-	AWS_SECRET_ACCESS_KEY=$(iniget /root/.aws/credentials $PROFILE aws_secret_access_key)
-	AWS_REGION=$(iniget /root/.aws/config $PROFILE region)
+	AWS_ACCESS_KEY_ID=$(iniget /.aws/credentials $PROFILE aws_access_key_id)
+	AWS_SECRET_ACCESS_KEY=$(iniget /.aws/credentials $PROFILE aws_secret_access_key)
+	AWS_REGION=$(iniget /.aws/config $PROFILE region)
 	terraform $@
 fi
 
