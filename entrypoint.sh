c6e3883121ac7ad494881d13c905288a8640bca6
Aldrin Baroi <aldrin.baroi@gmail.com>
2022-04-12 07:46:37 +0000

    For the time being, hardcode PROFILE value to "default"

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,11 @@ if [[ $CMD == "aws" ]]; then
 elif [[ $CMD == "console" && $ARG1 == "bash" ]]; then
 	bash
 else
-	AWS_ACCESS_KEY_ID=$(iniget /.aws/credentials $PROFILE aws_access_key_id)
-	AWS_SECRET_ACCESS_KEY=$(iniget /.aws/credentials $PROFILE aws_secret_access_key)
-	AWS_REGION=$(iniget /.aws/config $PROFILE region)
-	terraform $@
+	PROFILE=default
+	export AWS_ACCESS_KEY_ID=$(iniget /.aws/credentials $PROFILE aws_access_key_id)
+	export AWS_SECRET_ACCESS_KEY=$(iniget /.aws/credentials $PROFILE aws_secret_access_key)
+	export AWS_REGION=$(iniget /.aws/config $PROFILE region)
+	terraform -chdir=/root/work $@
 fi
 
 #::END::
