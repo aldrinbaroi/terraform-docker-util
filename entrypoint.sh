361f908fd99f6b0ede99c8b86e4b806daf1d3d94
Aldrin Baroi <aldrin.baroi@gmail.com>
2022-04-15 08:50:03 +0000

    Updated to keep work directory clean

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 CMD=$1
 ARG1=$2
+
+[[ ! -d $PLUGIN_CACHE_DIR ]] && mkdir -p $PLUGIN_CACHE_DIR
 if [[ $CMD == "aws" ]]; then
 	shift 1
 	aws $@
@@ -11,7 +13,7 @@ else
 	export AWS_ACCESS_KEY_ID=$(iniget /.aws/credentials $AWS_PROFILE aws_access_key_id)
 	export AWS_SECRET_ACCESS_KEY=$(iniget /.aws/credentials $AWS_PROFILE aws_secret_access_key)
 	export AWS_REGION=$(iniget /.aws/config $AWS_PROFILE region)
-	terraform -chdir=/root/work $@
+	terraform -chdir=/root $@
 fi
 
 #::END::
