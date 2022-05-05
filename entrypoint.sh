52db921c7bd6709eafb81bcd0e0598459192a175
Aldrin Baroi <aldrin.baroi@gmail.com>
2022-05-05 18:10:20 +0000

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 CMD=$1
 ARG1=$2
 
+export AWS_CONFIG_DIR=/.aws
 [[ ! -d $PLUGIN_CACHE_DIR ]] && mkdir -p $PLUGIN_CACHE_DIR
 if [[ $CMD == "aws" ]]; then
 	shift 1
@@ -10,9 +11,9 @@ if [[ $CMD == "aws" ]]; then
 elif [[ $CMD == "console" && $ARG1 == "bash" ]]; then
 	bash
 else
-	export AWS_ACCESS_KEY_ID=$(iniget /.aws/credentials $AWS_PROFILE aws_access_key_id)
-	export AWS_SECRET_ACCESS_KEY=$(iniget /.aws/credentials $AWS_PROFILE aws_secret_access_key)
-	export AWS_REGION=$(iniget /.aws/config $AWS_PROFILE region)
+	export AWS_ACCESS_KEY_ID=$(ini-get /.aws/credentials $AWS_PROFILE aws_access_key_id)
+	export AWS_SECRET_ACCESS_KEY=$(ini-get /.aws/credentials $AWS_PROFILE aws_secret_access_key)
+	export AWS_REGION=$(ini-get /.aws/config $AWS_PROFILE region)
 	terraform -chdir=/root $@
 fi
 
