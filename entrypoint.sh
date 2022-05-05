9773be0346744fa0d5e6b8d38e8e88ca0a4cecad
Aldrin Baroi <aldrin.baroi@gmail.com>
2022-05-05 18:20:05 +0000

    Updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ CMD=$1
 ARG1=$2
 
 export AWS_CONFIG_DIR=/.aws
-export AWS_ACCESS_KEY_ID=$(ini-get /.aws/credentials $AWS_PROFILE aws_access_key_id)
-export AWS_SECRET_ACCESS_KEY=$(ini-get /.aws/credentials $AWS_PROFILE aws_secret_access_key)
-export AWS_REGION=$(ini-get /.aws/config $AWS_PROFILE region)
+export AWS_ACCESS_KEY_ID=$(ini-get $AWS_CONFIG_DIR/credentials $AWS_PROFILE aws_access_key_id)
+export AWS_SECRET_ACCESS_KEY=$(ini-get $AWS_CONFIG_DIR/credentials $AWS_PROFILE aws_secret_access_key)
+export AWS_REGION=$(ini-get $AWS_CONFG_DIR/config $AWS_PROFILE region)
 [[ ! -d $PLUGIN_CACHE_DIR ]] && mkdir -p $PLUGIN_CACHE_DIR
 if [[ $CMD == "aws" ]]; then
 	shift 1
